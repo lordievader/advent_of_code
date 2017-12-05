d1df38c0524fe4dc199e5dcceb8ad8fb0dc506bd
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2017-12-05 09:47:28 +0100

    Add input and solution to day_1

---
+++ day_1/day1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def read_input():
+    with open('input', 'r') as input_file:
+        digits = input_file.read().replace('\n', '')
+
+    return digits
+
+def matches(digits):
+    summed = 0
+    for index, digit in enumerate(digits):
+        if index == 0:
+            previous = -1
+
+        else:
+            previous = index - 1
+
+        previous_digit = digits[previous]
+        if previous_digit == digit:
+            summed += int(digit)
+
+    return summed
+
+def main():
+    digits = read_input()
+    print(matches(digits))
+
+
+if __name__ == '__main__':
+    main()
