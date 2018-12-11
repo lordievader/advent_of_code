2165e140bfc02a430e34e671d49c1b8da5b30e5f
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2018-12-11 19:53:16 +0100

    Add documentation to day 05

--- 2018/day_05/part_2.py
+++ 2018/day_05/part_2.py
@@ -4,11 +4,27 @@ import part_1
 
 
 def find_length(letter, sequence):
+    """Removes the letter and runs the solution of part 1 to find the
+    resulting length.
+
+    :param letter: letter to remove
+    :type letter: str
+    :param sequence: input sequence
+    :type sequence: str
+    :return: length of the output
+    """
     modified_sequence = re.sub(letter, '', sequence, flags=re.I)
     return part_1.solution(modified_sequence)
 
 
 def remove_poly(sequence):
+    """Finds the shortest output by removing a letter and trying to
+    collapse the poly.
+
+    :param sequence: input sequence
+    :type sequence: str
+    :return: shortest length
+    """
     letters = set(list(sequence.lower()))
     length = None
     with concurrent.futures.ProcessPoolExecutor() as executor:
